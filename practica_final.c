bbb05ce615b6b56e444cdec5a2ba56afe5a3f2b8
rbecaa00 <rbecaa00@estudiantes.unileon.es>
2021-12-22 18:34:51 +0100

    Variables globales, señales y mutex

--- practica_final.c
+++ practica_final.c
@@ -7,6 +7,31 @@
 #include <signal.h>
 #include <ctype.h>
 
+pthread_mutex_t fichero;
+pthread_mutex_t colaClientes;
+pthread_mutex_t ascensor;
+pthread_mutex_t maquinas;
+
+struct cliente{
+	int id;
+	int atentido; // Tipo boolean 0 no atendido 1 atendido
+	int tipo; // Tipo boolean 0 no vip 1 vip
+	int ascensor; 
+};
+
+struct cliente *clientes;
+
+int numClientes = 20;
+int contClientes = 0;
+int numCliAscensor = 0;
+
+
+char logFileName[] = "Practica_Final.log";
+
+FILE *logFile;
+
+char ids[20];
+
 void nuevoCliente();
 void accionesCliente();
 void accionesRecepcionista();
@@ -16,7 +41,74 @@ void writeLogMessage(char *id, char *msg);
  * Implementado por Rubén Bécares Álvarez
  */
 int main(int argc, char* argv[]){
+	
+	sprintf(ids, "%d", getpid());
+	writeLogMessage("Main", "Inicio programa");
 
+	/**
+	 * Montado de señales
+	 * 
+	 */
+	struct sigaction sNV;
+	sNV.sa_handler = nuevoCliente;
+	if(-1 == sigaction(SIGUSR1, &sNV, NULL)){
+		//sprintf(ids, "%d", getpid());
+		writeLogMessage("Main", "Fallo montando la señal de no vips");
+		perror("Fallo montando la señal de no vips");
+		return 1;
+	}else{
+		//sprintf(ids, "%d", getpid());
+		writeLogMessage("Main", "Se ha montando la señal de no vips");
+	}
+
+	struct sigaction sV;
+	sV.sa_handler = nuevoCliente;
+	if(-1 == sigaction(SIGUSR2, &sV, NULL)){
+		//sprintf(ids, "%d", getpid());
+		writeLogMessage("Main", "Fallo montando la señal de vips");
+		perror("Fallo montando la señal de vips");
+		return 1;
+	}else{
+		//sprintf(ids, "%d", getpid());
+		writeLogMessage("Main", "Se ha montando la señal de vips");
+	}
+
+	struct sigaction sC;
+	sC.sa_handler = nuevoCliente;
+	if(-1 == sigaction(SIGINT, &sC, NULL)){
+		//sprintf(ids, "%d", getpid());
+		writeLogMessage("Main", "Fallo montando la señal de terminar");
+		perror("Fallo montando la señal de terminar");
+		return 1;
+	}else{
+		//sprintf(ids, "%d", getpid());
+		writeLogMessage("Main", "Se ha montando la señal de terminar");
+	}
+
+	/**
+	 * Inicializacion de recursos
+	 * 
+	 */
+
+	clientes = (struct cliente *)malloc(sizeof(struct cliente)*numClientes);
+	if (pthread_mutex_init(&fichero, NULL)!=0){
+		exit(-1); 
+	}
+	if (pthread_mutex_init(&colaClientes, NULL)!=0){
+		exit(-1); 
+	}
+	if (pthread_mutex_init(&ascensor, NULL)!=0){
+		exit(-1); 
+	}
+	if (pthread_mutex_init(&maquinas, NULL)!=0){
+		exit(-1); 
+	}
+	
+	
+	free(clientes);
+
+	//sprintf(ids, "%d", getpid());
+	writeLogMessage("Main", "Fin programa");
 	return 0;
 }
 
@@ -24,11 +116,11 @@ void nuevoCliente(){
 	
 }
 
-
+//Hilo
 void accionesCliente(){
 	//Victor escribe aqui
 }
-
+//Hilo
 void accionesRecepcionista(){
 }
 

ee7c4d4b44b64065feb214ea2ac7188ec1549d2b
rbecaa00 <rbecaa00@estudiantes.unileon.es>
2021-12-27 14:03:40 +0100

    Fin del desarrollo del main

--- practica_final.c
+++ practica_final.c
@@ -1,4 +1,4 @@
-#include <stdio.h>	
+#include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <stdlib.h>
@@ -6,6 +6,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 #include <ctype.h>
+#include <string.h>
 
 pthread_mutex_t fichero;
 pthread_mutex_t colaClientes;
@@ -15,18 +16,19 @@ pthread_mutex_t maquinas;
 struct cliente{
 	int id;
 	int atentido; // Tipo boolean 0 no atendido 1 atendido
-	int tipo; // Tipo boolean 0 no vip 1 vip
-	int ascensor; 
+	int tipo;	  // Tipo boolean 0 no vip 1 vip
+	int ascensor;
 };
 
 struct cliente *clientes;
+int *maquinasCheckin;
 
-int numClientes = 20;
-int contClientes = 0;
-int numCliAscensor = 0;
+int numClientes;
+int contClientes;
+int numCliAscensor;
+int numMaquinas;
 
-
-char logFileName[] = "Practica_Final.log";
+char logFileName[19];
 
 FILE *logFile;
 
@@ -40,98 +42,127 @@ void writeLogMessage(char *id, char *msg);
 /**
  * Implementado por Rubén Bécares Álvarez
  */
-int main(int argc, char* argv[]){
-	
-	sprintf(ids, "%d", getpid());
+int main(int argc, char *argv[]){
+	/**
+	 * Inicialización nombre fichero
+	 *
+	 */
+	strcpy(logFileName, "Practica_Final.log");
+
+	// sprintf(ids, "%d", getpid());
 	writeLogMessage("Main", "Inicio programa");
 
 	/**
 	 * Montado de señales
-	 * 
+	 *
 	 */
 	struct sigaction sNV;
 	sNV.sa_handler = nuevoCliente;
-	if(-1 == sigaction(SIGUSR1, &sNV, NULL)){
-		//sprintf(ids, "%d", getpid());
+	if (-1 == sigaction(SIGUSR1, &sNV, NULL)){
+		// sprintf(ids, "%d", getpid());
 		writeLogMessage("Main", "Fallo montando la señal de no vips");
 		perror("Fallo montando la señal de no vips");
 		return 1;
 	}else{
-		//sprintf(ids, "%d", getpid());
+		// sprintf(ids, "%d", getpid());
 		writeLogMessage("Main", "Se ha montando la señal de no vips");
 	}
 
 	struct sigaction sV;
 	sV.sa_handler = nuevoCliente;
-	if(-1 == sigaction(SIGUSR2, &sV, NULL)){
-		//sprintf(ids, "%d", getpid());
+	if (-1 == sigaction(SIGUSR2, &sV, NULL)){
+		// sprintf(ids, "%d", getpid());
 		writeLogMessage("Main", "Fallo montando la señal de vips");
 		perror("Fallo montando la señal de vips");
 		return 1;
 	}else{
-		//sprintf(ids, "%d", getpid());
+		// sprintf(ids, "%d", getpid());
 		writeLogMessage("Main", "Se ha montando la señal de vips");
 	}
 
 	struct sigaction sC;
 	sC.sa_handler = nuevoCliente;
-	if(-1 == sigaction(SIGINT, &sC, NULL)){
-		//sprintf(ids, "%d", getpid());
+	if (-1 == sigaction(SIGINT, &sC, NULL)){
+		// sprintf(ids, "%d", getpid());
 		writeLogMessage("Main", "Fallo montando la señal de terminar");
 		perror("Fallo montando la señal de terminar");
 		return 1;
 	}else{
-		//sprintf(ids, "%d", getpid());
+		// sprintf(ids, "%d", getpid());
 		writeLogMessage("Main", "Se ha montando la señal de terminar");
 	}
 
 	/**
 	 * Inicializacion de recursos
-	 * 
+	 *
 	 */
 
-	clientes = (struct cliente *)malloc(sizeof(struct cliente)*numClientes);
-	if (pthread_mutex_init(&fichero, NULL)!=0){
-		exit(-1); 
+	numClientes = 20;
+	contClientes = 0;
+	numCliAscensor = 0;
+	numMaquinas = 5;
+
+	clientes = (struct cliente *)malloc(sizeof(struct cliente) * numClientes);
+	maquinasCheckin = (int *)malloc(sizeof(int) * numMaquinas);
+
+	for (int i = 0; i < numClientes; i++){
+		clientes[i].id = 0;
+		clientes[i].tipo = 0;
+		clientes[i].atentido = 0;
+		clientes[i].ascensor = 0;
 	}
-	if (pthread_mutex_init(&colaClientes, NULL)!=0){
-		exit(-1); 
+
+	if (pthread_mutex_init(&fichero, NULL) != 0){
+		exit(-1);
 	}
-	if (pthread_mutex_init(&ascensor, NULL)!=0){
-		exit(-1); 
+	if (pthread_mutex_init(&colaClientes, NULL) != 0){
+		exit(-1);
 	}
-	if (pthread_mutex_init(&maquinas, NULL)!=0){
-		exit(-1); 
+	if (pthread_mutex_init(&ascensor, NULL) != 0){
+		exit(-1);
 	}
-	
-	
+	if (pthread_mutex_init(&maquinas, NULL) != 0){
+		exit(-1);
+	}
+
+	pthread_t recepcionista_1, recepcionista_2, recepcionista_3;
+	int r1[] = {0, 1};
+	int r1[] = {0, 2};
+	int r1[] = {1, 3};
+	pthread_create(&recepcionista_1, NULL, accionesRecepcionista, 0);
+	pthread_create(&recepcionista_2, NULL, accionesRecepcionista, 0);
+	pthread_create(&recepcionista_3, NULL, accionesRecepcionista, 1);
+
+	while (1){
+		pause();
+	}
+
 	free(clientes);
 
-	//sprintf(ids, "%d", getpid());
+	// sprintf(ids, "%d", getpid());
 	writeLogMessage("Main", "Fin programa");
 	return 0;
 }
 
 void nuevoCliente(){
-	
 }
 
-//Hilo
+// Hilo
 void accionesCliente(){
-	//Victor escribe aqui
+	// Victor escribe aqui
 }
-//Hilo
+// Hilo
 void accionesRecepcionista(){
 }
 
-void writeLogMessage(char *id, char *msg) {
+void writeLogMessage(char *id, char *msg){
 	// Calculamos la hora actual
-	 time_t now = time(0);
-	 struct tm *tlocal = localtime(&now);
-	 char stnow[25];
-	 strftime(stnow, 25, "%d/%m/%y %H:%M:%S", tlocal);
+	time_t now = time(0);
+	struct tm *tlocal = localtime(&now);
+	char stnow[25];
+	strftime(stnow, 25, "%d/%m/%y %H:%M:%S", tlocal);
 	// Escribimos en el log
-	 logFile = fopen(logFileName, "a");
-	 fprintf(logFile, "[%s] %s: %s\n", stnow, id, msg);
-	 fclose(logFile);
- }
+	logFile = fopen(logFileName, "a");
+	fprintf(logFile, "[%s] %s: %s\n", stnow, id, msg);
+	fclose(logFile);
+}
